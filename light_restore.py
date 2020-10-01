391538b5c66f681afaaecde8e874e56093a34b82
Patrick Shinn <shinn16@users.noreply.github.com>
2020-10-01 19:37:35 -0400

    User can now provide a time range in which all lights will be turned off regards of their past state.

--- light_restore.py
+++ light_restore.py
@@ -1,5 +1,15 @@
 import appdaemon.plugins.hass.hassapi as hass
-from datetime import timedelta
+from datetime import datetime, timedelta
+
+# print utilites (thanks geeks for geeks: https://www.geeksforgeeks.org/print-colors-python-terminal/)
+def prRed(skk): return("\033[91m{}\033[00m".format(skk)) 
+def prGreen(skk): return("\033[92m{}\033[00m".format(skk)) 
+def prYellow(skk): return("\033[93m{}\033[00m".format(skk)) 
+def prLightPurple(skk): return("\033[94m{}\033[00m".format(skk)) 
+def prPurple(skk): return("\033[95m{}\033[00m".format(skk)) 
+def prCyan(skk): return("\033[96m{}\033[00m".format(skk)) 
+def prLightGray(skk): return("\033[97m{}\033[00m".format(skk)) 
+def prBlack(skk): return("\033[98m{}\033[00m".format(skk)) 
 
 class LightRestore(hass.Hass):
     """
@@ -7,7 +17,7 @@ class LightRestore(hass.Hass):
     you have smart bulbs that do not have a power on behaviour that will come back to an 'on' state
     after a power outage.
     """
-    VERSION = '0.1'
+    VERSION = '0.2'
 
     def initialize(self):
         """
@@ -33,6 +43,17 @@ class LightRestore(hass.Hass):
         else:
             self.up_time_sensor = "sensor.time_online"
 
+        # check a time range if one is provided for night time hours. If there is a range, get it.
+        # if the server restarts in this time range, we will just turn everything to an off state.
+        self.check_time = False
+        if "night_range" in self.args:
+            try:
+                self.start_time = self.args['night_range']['start_time']
+                self.end_time   = self.args['night_range']['end_time']
+                self.check_time = True
+            except KeyError as e:
+                self.log("key missing in settings file: {}".format(str(e)), level="ERROR")
+
         #  only run once
         self.restore_lights()
         # TODO add switches option?
@@ -43,14 +64,6 @@ class LightRestore(hass.Hass):
         as defined in the history db.
         """
         self.log("Restoring Previous Light States")
-
-        # calculate server start time
-        current_time = self.datetime(aware=False)  # We do not want tz aware, HA stores entites in times as UTC
-        self.log("Current Time: {}".format(current_time))
-
-        hass_up_time = timedelta(seconds=self.calculate_up_time(self.get_state(self.up_time_sensor, attribute='all')))
-        server_start_time = current_time - hass_up_time
-        self.log("Hass start time: {}".format(server_start_time))
 
         # get light entites
         lights = self.get_state("light")
@@ -71,11 +84,27 @@ class LightRestore(hass.Hass):
 
         # remove the groups
         lights = tuple(set(lights).difference(set(group)))
+
+        # check to see if we have a nighttime range, if so, forget the states. Turn everything off
+        if self.check_time:
+            if self.now_is_between(self.start_time, self.end_time):
+                self.log("The current time is within the defined night time range, turning all lights off")
+                for light in lights:
+                    self.turn_off(light)
+                    self.log("{:25} => {}".format(light.split(".")[1], prRed("off")))
+                return # we are done.
+        
+        # calculate server start time since we are not within the time range to turn everything off
+        current_time = self.datetime(aware=False)  # We do not want tz aware, HA stores entites in times as UTC
+        self.log("Current Time: {}".format(current_time))
+
+        hass_up_time = timedelta(seconds=self.calculate_up_time(self.get_state(self.up_time_sensor, attribute='all')))
+        server_start_time = current_time - hass_up_time
+        self.log("Hass start time: {}".format(server_start_time))
         
         # now get the history of each light, before the server restarted
         light_restore_state = dict()
         for light in lights:
-            
             try:
                 # This is a double nested list for some reason. There is only one element in the outer list, so [0] gets the list of states
                 light_states = self.get_history(entity_id=light, endtime=server_start_time, days=self.days)[0]
@@ -93,11 +122,12 @@ class LightRestore(hass.Hass):
         # apply light states
         self.log("Restoring lights to previous states:")
         for light in light_restore_state:
-            self.log("{:25} => {}".format(light.split(".")[1], light_restore_state[light]))
             if light_restore_state[light] == 'on':
                 self.turn_on(light)
+                self.log("{:25} => {}".format(light.split(".")[1], prGreen(light_restore_state[light])))
             else:
                 self.turn_off(light)
+                self.log("{:25} => {}".format(light.split(".")[1], prRed(light_restore_state[light])))
 
 
     def calculate_up_time(self, up_time_dict: dict) -> int:
@@ -126,6 +156,4 @@ class LightRestore(hass.Hass):
 
         restart_time_offset = round(scale_factor * float(up_time_dict['state']))
         self.log("Calulated start time offest: {} seconds".format(restart_time_offset))
-
-
         return restart_time_offset
